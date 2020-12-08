075e5507c85ba41af73eb7e136caa1094b076852
Rahul Krishna Penta <66423677+rahul0082@users.noreply.github.com>
2020-12-08 23:55:07 +0530

    Update LoginModel.kt

--- getPostApp/app/src/main/java/com/rahul/getpostapp/Models/LoginModel.kt
+++ getPostApp/app/src/main/java/com/rahul/getpostapp/Models/LoginModel.kt
@@ -1,11 +1,12 @@
 package com.rahul.getpostapp.Models
 
-class LoginModel(username:String, email:String, password:String,is_student:Boolean, is_instructor:Boolean, registration_token:String?) {
+class LoginModel(username:String, email:String, password:String,is_student:Boolean, is_instructor:Boolean,is_ta:Boolean, registration_token:String?) {
     val username:String
     private val email:String
     private val password:String
     private val is_student:Boolean
     private val is_instructor:Boolean
+    private val is_ta:Boolean
     private val registration_token:String?
 
     init{
@@ -14,6 +15,7 @@ class LoginModel(username:String, email:String, password:String,is_student:Boole
         this.password=password
         this.is_student=is_student
         this.is_instructor=is_instructor
+        this.is_ta = is_ta
         this.registration_token=registration_token
     }
 }
